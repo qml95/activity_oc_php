ae9b0596d39b0d286015dcbff07e132616bf2b0e
qml95 <qml@wedigital.garden>
2018-12-22 22:37:35 +0100

    feature init PHP connect BDD with variable name

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -1,13 +1,28 @@
 <?php
-// connect BDD
+$db_config = array();
+$db_config['DRIVER']        = 'mysql';
+$db_config['HOST']          = 'localhost';
+$db_config['DB_NAME']       = 'minichat';
+$db_config['USER']          = 'root';
+$db_config['PASSWORD']      = 'root';
+$db_config['OPTIONS']       = array(
+                                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
+                                   );
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+    // connect BDD
+    $bdd = new PDO($db_config['DRIVER'] .':host='. $db_config['HOST'] .';dbname='. $db_config['DB_NAME'],
+                   $db_config['USER'],
+                   $db_config['PASSWORD'],
+                   $db_config['OPTIONS']
+                   );
 }
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
+catch (Exception $e) {
+    echo 'Erreur : ' . $e->getMessage();
+    die();
 }
+
 
 // start session
 session_start();
